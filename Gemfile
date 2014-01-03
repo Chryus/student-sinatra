5c0e8537dab6537e546c924045d937876e82e4cb
Chris <chris.haack@me.com>
2014-01-03 15:00:24 -0500

    add transition

--- Gemfile
+++ Gemfile
@@ -1,3 +1,19 @@
-source 'https://rubygems.org'
+source 'https://rubygems.org'#same as :rubygems
 
 gem 'sinatra', require: 'sinatra/base'
+
+group :development do #run these only during development
+	gem "shotgun" #allows us to not have to restart our server
+	gem "tux" #allows us to have a console for our app, like a debugger for an if __FILE__ == $PROGRAM_NAME
+	gem "debugger"
+end 
+
+# source "http://rubygems.org"
+
+# gem 'sinatra'
+
+# group :development do
+#   gem 'tux'
+#   gem 'shotgun'
+#   gem 'debugger'
+# end
--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require './lib/thing'
 
 # Why is it a good idea to wrap our App class in a module?
 module StudentSite
@@ -6,5 +7,17 @@ module StudentSite
     get '/' do
       "hello world!"
     end
+
+    get '/hello-world' do
+      @thing = Thing.new("Magical Jasper", 20)
+    	@random_numbers = (1..20).to_a.shuffle
+    	erb :hello #erb is a method and we're calling it on the hello view, which returns a string
+    end
+
+    get '/artists' do
+      @artists = ["Kembra", "Charles Atlas", "Michael Clark", "Marina"]
+    	erb :artists
+    end
+
   end
 end
